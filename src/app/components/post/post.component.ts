7ab2ededacd926d8ee77538a90fcf3a99c44e073
William Price <wbprice3@yahoo.com>
2022-12-11 20:01:52 -0600

    Working on Like Button

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -5,7 +5,11 @@ import Post from "src/app/models/Post";
 import User from "src/app/models/User";
 import { AuthService } from "src/app/services/auth.service";
 import { PostService } from "src/app/services/post.service";
+import { LikesModel } from "src/app/models/likes";
 import "leo-profanity";
+import { HttpClient } from "@angular/common/http";
+import { LikesService } from "src/app/services/likes.service";
+import { resolveSoa } from "dns";
 
 @Component({
 	selector: "app-post",
@@ -27,6 +31,7 @@ export class PostComponent implements OnInit {
 
 	@Input()
 	currentUser: User;
+	likeModel: any = {};
 	public set value(user: User) {
 		this.currentUser = user;
 	}
@@ -36,10 +41,60 @@ export class PostComponent implements OnInit {
 
 	constructor(
 		private postService: PostService,
-		private authService: AuthService
+		private authService: AuthService,
+		private http : HttpClient,
+		private likes : LikesService
 	) {}
 
-	ngOnInit(): void {}
+
+	
+	palleteColor = "primary";	
+	likecount? : number;
+	canLike? : boolean;
+	postid? : number;
+		getLikes(){
+	this.http.get(`http://localhost:8080/likes/getlikes/${this.postid}`, {withCredentials: true ,observe : "response"}).subscribe(
+			  (res : any ) => {
+				console.log(this.postid);
+				console.log(res.body);
+				this.likecount = res.body.length;
+				
+			  },
+			  err => {
+				console.log(err);
+			  }
+			 )
+			}
+	
+	change(){
+		this.palleteColor = "warn"
+		
+		
+		this.likeModel.likeCount = 1;
+		this.likeModel.likedBy = this.currentUser.email;
+		this.likeModel.postID = this.post.id;
+		this.postid = this.post.id;
+		this.likes.updateLikes(this.likeModel).subscribe
+		((data) => {
+			console.log(data)
+			this.getLikes();
+		},
+
+			(error) => {
+				console.log(error);
+				
+			}
+	)}
+
+	
+
+	ngOnInit(): void {
+		
+	}
+
+	ngOnChanges(){
+		
+	}
 
 	toggleReplyToPost = () => {
 		this.replyToPost = !this.replyToPost;
